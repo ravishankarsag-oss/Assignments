280b63c24ca393b29c794f1a0f5fc0296ec76fa7
Ravi Shankar <ravishankar.sag@gmail.com>
2026-01-09 10:13:58 +0530

    Initial program

---
+++ javascript-training/assignments/assignment1.js
@@ -0,0 +1,97 @@
+//Assignment1
+//1.Temperature of a city in degrees Celsius: 25.5
+
+let tempratureOfCity=25.5;
+console.log("Temprature of city is :",tempratureOfCity);
+
+//2.Whether a customer has placed an order: true or false
+
+let customerOrder=true;
+console.log("Customer had placed an order =",customerOrder);
+
+//3.Person's phone number: "123-456-7890"
+
+let phoneNum= "123-456-7890";
+console.log("My phone number is:",phoneNum);
+
+//4.Amount of money in a customer's bank account: 1000.50
+
+let customerBalance=1000.50
+console.log("Custome account balance is :",customerBalance);
+
+//5.Person's email address: "john.doe@example.com"
+
+let emailId="john.doe@example.com";
+console.log("Person email id is: ",emailId);
+
+//6.Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+
+let cordinates="37.7749, -122.4194";
+console.log("Cordinate of the location is :",cordinates);
+
+//7.Person's marital status: true or false
+let maritalStatus=true
+console.log("Martial status of person married is :",maritalStatus);
+//8.Person's occupation: "Software Engineer"
+
+let occupation="Software Engineer";
+console.log("Occupation of Person is :",occupation);
+
+//9.Person's favourite colour: "Blue"
+
+let fColour="Blue";
+console.log("Fevourite colour of person is :",fColour);
+
+//10.Current year: 2023
+
+let cYear=2023;
+console.log("Current year is :",cYear);
+
+//11.Number of followers on a social media platform: 1,000,000
+
+let socilMediaFollower="1,000,000"
+console.log("Number of follower on social media are: ",socilMediaFollower);
+
+//12.Rating of a movie: 7.5
+
+let movieRating =7.5;
+console.log("Rating of movie is :",movieRating);
+
+//13.Person's blood type: 'A'
+
+let bloodGroup='A';
+console.log("Blood group of person is :",bloodGroup);
+
+//14.Title of a book: "To Kill a Mockingbird"
+
+let bookTittle= "To Kill a Mockingbird";
+console.log("Tittle of book is :",bookTittle);
+
+//15.Number of employees in a company: 500
+let empCount=500;
+console.log("Number of employee in company are :",empCount);
+
+//16.Time of an event: 2:30
+
+let eventTime="2:30 PM";
+console.log("Time of event is :",eventTime);
+
+//17.Name of a country: "United States"
+
+let country="United States";
+console.log("Country name is :",country);
+
+//18.Person's eye color: "Brown"
+
+let eyeColor="Brown";
+console.log("Person's eye color is :",eyeColor);
+
+//19.Person's birthplace: "New York City"
+
+let birthplace="New York City";
+console.log("Person's birthpalce is :",birthplace);
+
+//20.Distance between two cities: 200.5
+
+let distance=200.5;
+console.log("Distance between two city is :",distance);
